4f479a038b16c588d4ebf1ec63c284a070351ee0
simarjeet <simarjeet.sm7@gmail.com>
2024-08-16 21:38:12 +0530

    build the app

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@
 
 import { About } from "@/components/about-me";
 import { DotBackground } from "@/components/background";
-import { BentoProject } from "@/components/bento-project";
-import { Contact } from "@/components/contact";
+// import { BentoProject } from "@/components/bento-project";
+// import { Contact } from "@/components/contact";
 import { CustomBackground } from "@/components/custom-background";
 import { LayoutGridProject } from "@/components/layout-project";
-import { Project } from "@/components/project";
+
 import { Skills } from "@/components/skills-card";
 import { SparklesEnding } from "@/components/sparkles-bg";
 import Image from "next/image";
--- components/about-me.tsx
+++ components/about-me.tsx
@@ -1,49 +1,44 @@
 "use client"
 import { useActiveSection } from "@/hooks/use-active-section";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 import { useEffect } from "react";
 import { useInView } from "react-intersection-observer";
 
-export const About=()=>{
-  const {ref, inView} = useInView({
-    threshold:0.5,
+export const About = () => {
+  const { ref, inView } = useInView({
+    threshold: 0.5,
   });
-  const {setActive} = useActiveSection();
-  
-  useEffect(()=>{
+  const { setActive } = useActiveSection();
+
+  useEffect(() => {
     if (inView) setActive('About');
-  },[inView,setActive]);
+  }, [inView, setActive]);
 
-    return(
-       <motion.div
-       initial ={{y:-100, opacity:0}}
-       animate = {{y:0,opacity:1}} id="about" ref={ref} className="flex items-center justify-center scroll-mt-32">
-         <motion.div
-        
-    
-        className="flex-col font-mono font-semibold flex items-center justify-center  mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28 z-5 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
-            <h2 className="text-5xl pb-2">
-                About Me 
-            </h2>
-            <p className="mb-3 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
-        I{`'`}m Simarjeet Singh Mehra currently doing my bachelors in Electrical Engineering{" "}
-        <span className="font-medium">from Delhi Technological University</span>, I have a strong foundation in fullstack web development
-        {" "}
-        <span className="font-medium">full-stack web development</span>.{" "}
-        <span className="">I have developed several projects in MERN Stack which you can find in the below sections.</span>I have a proficiency in <span className="underline">JavaScript, TypeScript, C++, Python,</span>
-        {" "}
-        
-        and frameworks like <span className="font-medium">React.js, Node.js, Express.js, and Tailwind CSS.
-        </span>
-        . I am also familiar with Prisma and PostgresSql. I am also good with databases and making scalable backend application.
-      </p>
+  return (
+    <motion.div
+      initial={{ y: -100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      id="about"
+      ref={ref}
+      className="flex items-center justify-center scroll-mt-32"
+    >
+      <motion.div
+        className="flex-col font-mono font-semibold flex items-center justify-center mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28 z-5 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500"
+      >
+        <h2 className="text-5xl pb-2">
+          About Me 
+        </h2>
+        <p className="mb-3 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
+          I&apos;m Simarjeet Singh Mehra currently doing my bachelors in Electrical Engineering
+          <span className="font-medium"> from Delhi Technological University</span>. I have a strong foundation in 
+          <span className="font-medium"> full-stack web development</span>. I have developed several projects in MERN Stack which you can find in the below sections. I have proficiency in <span className="underline">JavaScript, TypeScript, C++, Python</span>, and frameworks like <span className="font-medium">React.js, Node.js, Express.js, and Tailwind CSS</span>. I am also familiar with Prisma and PostgreSQL. I am also skilled in databases and creating scalable backend applications.
+        </p>
 
-      <p className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
-        <span className="italic">I'm currently looking for a </span> full-time position
-        as a software developer{" "}
-        
-      </p>
-        </motion.div>
-       </motion.div>
-    )
-}
+        <p className="bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500">
+          <span className="italic">I&apos;m currently looking for a </span> full-time position
+          as a software developer.
+        </p>
+      </motion.div>
+    </motion.div>
+  );
+};
